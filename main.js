9f82a7dc465280c1629ab08dbf18b3351c507591
Chad Cornwell <chadcornwell86@yahoo.com>
2022-03-27 12:39:54 -0500

    something something complete

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 var countSheep = function (num){
     //your code here
     let str = ""
+    if( num === 0){
+        console.log("")  
+    }
+    else{
     for (i = 1; i <= num; i++){
-      str += str + `${i} sheep...`
+      str += `${i} sheep...`
       
     }
     console.log(str)
-  }
+  }};
   countSheep(5)
