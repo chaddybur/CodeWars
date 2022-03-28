f7d512496d95672ebd2f4e100d6198f4a6e57695
Chad Cornwell <chadcornwell86@yahoo.com>
2022-03-28 09:01:25 -0500

    FIRST TRY?

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-function boolToWord( bool ){
-    //...
-    if(bool === true){
-      return 'Yes'
-    }else{
-      return 'No'
+const flip=(d, a)=>{
+    //TODO
+    if(d === "R"){
+      a.sort((a,b) => a-b)
     }
+    else{
+        a.sort((a,b) => b-a)
+    }
+    
+    console.log(a)
   }
+
+  flip("L", [3,5,2,1,6,9])
