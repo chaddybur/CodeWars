4a72f16d1787a7f3955071b4c977704a624a57ed
Chad Cornwell <chadcornwell86@yahoo.com>
2022-03-29 15:18:08 -0500

    typo

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-function mergeArrays(arr1, arr2) {
-    
-    arr2.forEach(element => arr1.push(element))
-    
-    console.log(arr1)
-    let ans = arr1.sort((a,b) => a-b )
-
+function repeatStr(n, s) {
+  let ans = ""
+  for(i = 1; i <= n; i++){
+    ans += s
   }
+  console.log(ans);
+}
 
-  mergeArrays([1,2,3,4], [5,6,7,8])
+repeatStr(5, "s")
