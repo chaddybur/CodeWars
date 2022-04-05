2dc0dd86760a09f664ff2580c5e77335913a1b06
Chad Cornwell <chadcornwell86@yahoo.com>
2022-04-04 20:33:44 -0500

    FAILURE

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
-/* KATA NAME SHORTEST NAME <7 KYU>
+/* KATA NAME: JADEN CASING STRINGS <7 KYU>
 
-Simple, given a string of words, return the length of the shortest word(s).
-String will never be empty and you do not need to account for different data types.*/
+For simplicity, 
+you'll have to capitalize each word, check out how contractions are expected to be in the example below.
 
-// 
+Your task is to convert strings to how they would be written by Jaden Smith. 
+The strings are actual quotes from Jaden Smith, but they are not capitalized in 
+the same way he originally typed them.
 
-function findShort(s){
-  let newArr = s.split(" ")
+Example:
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"*/
+
+function toJadenCase(str){ 
+  //separate each word into an array
+  let newArr = str.split(" ")
   console.log(newArr)
-  let newNewArr = []
-  newArr.forEach(e => newNewArr.push(e.length))
+  // separate each letter in each word into an array
+  let newNewArr = newArr.map(e => e.split(""))
   console.log(newNewArr)
-  let newerNewArr = newNewArr.sort((a,b)=> a-b)
-  console.log(newerNewArr)
-  console.log(newNewArr[0])
+  // uppercase each letter[0] TAKE 2 SECOND TRY! FUCk.
+  let answer = ""
+  for(i = 0; i < newNewArr.length; i++){
+    newNewArr[i][0] = newNewArr[i][0].toUpperCase()
+    console.log(newNewArr[i])
+    answer += newNewArr[i].join("") + " "
+  }
+  console.log(answer)
+  
+  // join letter and then words
 
-}
 
-findShort("bitcoin take over the world maybe who knows perhaps")
-// Should return 3
+};
+
+toJadenCase("What the hell is wrong with this codewars?")
