b413a514700799d9cc43af2a7dbb1becf3e0c8e8
Chad Cornwell <chadcornwell86@yahoo.com>
2022-04-04 19:52:29 -0500

    FIRST TRYgit add .

--- main.js
+++ main.js
@@ -1,41 +1,21 @@
-/* NAME OF KATA: Exes and Ohs <7 KYU>
-XO("ooxx") => true
-XO("xooxx") => false
-XO("ooxXm") => true
-XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
-XO("zzoo") => false*/
+/* KATA NAME SHORTEST NAME <7 KYU>
 
-//STEPS
-// make everything lowercase
-function XO(str) {
-  let lowStrang = str.toLowerCase();
-  console.log(lowStrang)
-// turn string into array?
-let lowStrangArr = lowStrang.split("");
-console.log(lowStrangArr)
+Simple, given a string of words, return the length of the shortest word(s).
+String will never be empty and you do not need to account for different data types.*/
 
-//have a counter for x and a counter for o
-let counterO = 0
-let counterX = 0
-for(i = 0; i < lowStrangArr.length; i++){
-  if(lowStrangArr[i] === 'o'){
-    counterO += 1
-  }else if(lowStrangArr[i] === 'x'){
-    counterX += 1
-  }
-}
-if(counterO === counterX){
-  console.log(true)
-}else{
-  console.log(false)
+// 
+
+function findShort(s){
+  let newArr = s.split(" ")
+  console.log(newArr)
+  let newNewArr = []
+  newArr.forEach(e => newNewArr.push(e.length))
+  console.log(newNewArr)
+  let newerNewArr = newNewArr.sort((a,b)=> a-b)
+  console.log(newerNewArr)
+  console.log(newNewArr[0])
+
 }
 
-//check to see if both counters are the same
-//return true if they are the same
-//return false if they are not the same
-}
-XO("ooxx") 
-XO("xooxx") 
-XO("ooxXm") 
-XO("zpzpzpp") 
-XO("zzoo")
+findShort("bitcoin take over the world maybe who knows perhaps")
+// Should return 3
