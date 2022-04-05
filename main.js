64a9d095f81dd48b6d5786a639ef4f6d23a93bf0
Chad Cornwell <chadcornwell86@yahoo.com>
2022-04-05 10:48:02 -0500

    FIRST TRY WITH HELP

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 /* CODE WARS: WHO LIKE IT? <6 KYU>
 
-Implement the function which takes an array containing the names of people that like an item. 
-It must return the display text as shown in the examples:
 
-[]                                -->  "no one likes this"
-["Peter"]                         -->  "Peter likes this"
-["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
-["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
-["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 */
+// 8. Reduce Exercise
+    // Sum up the instances of each of these
+    /*const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck', 'pogostick'];
 
-//Steps.Long way? 
-// loop through the names array and add if statements for numbers? just if statements? No for loop?
-
-function likes(names) {
-
-      if(names.length === 0){
-        return `no one likes this`
-
-      }else if(names.length === 1){
-        return `${names[0]} likes this`
-
-      }else if(names.length === 2){
-        return `${names[0]} and ${names[1]} like this`
-      }else if(names.length === 3){
-        return `${names[0]}, ${names[1]} and ${names[2]}like this`
-      }else{
-        return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+    const transportation = data.reduce(function(obj, item) {
+      if (!obj[item]) {
+        obj[item] = 0;
       }
+      obj[item]++;
+      return obj;
+    }, {});
+
+    console.log(transportation); 
     
-  
-}
+    HAD TO LOOK UP AGAIN HOW TO USE REDUCE TO COUNT. ADDED TO ANKI*/
+
+    function count (string) {  
+      newArr = string.split("")
+
+      let answer = newArr.reduce(function(obj, item){
+        if(!obj[item]){
+          obj[item] = 0 
+        }
+        obj[item]++;
+        return obj;
+      }, {})
+      // The function code should be here
+       return answer;
+    }
