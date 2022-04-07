b5682924f89218f0463fd4d5da4407c9a6854ba1
Chad Cornwell <chadcornwell86@yahoo.com>
2022-04-07 17:31:20 -0500

    code war

--- main.js
+++ main.js
@@ -1,44 +1,15 @@
-/* CODE WARS: Highest Scoring Word <6 KYU>
-Given a string of words, you need to find the highest scoring word.
-Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
-You need to return the highest scoring word as a string.
-If two words score the same, return the word that appears earliest in the original string.
-All letters will be lowercase and all inputs will be valid.
-*/
-//PREP new to assign each lower case letter a value.
-// value 0 = 0, then each letter should be 1,2,3 etc.
 
-function high(x){
-
-let alpha = [0,"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", 
-"s", "t", "u", "v", "w", "x", "y", "z"]
-
-let newArr = x.split(" ")
 let newNewArr = []
-let number = []
-  newArr.forEach(e => newNewArr.push(e.split("")))
-
-  for(i = 1; i < alpha.length; i++){
-    for(j = 0; j < newNewArr.length; j++){
-      for(k = 0; k < newNewArr[j].length; k++){
-        if(newNewArr[j][k] === alpha[i]){
-          newNewArr[j][k] = i
-        } 
-        
-      }
-    
-      
-    
+function removeEveryOther(arr){
+  //your code here
+  let newArr = []
+  for(i=0; i < arr.length; i++){
+    if(i % 2 === 0){
+      newArr.push(arr[i])
     }
     
-    }
-    console.log(newNewArr)
-  
-  for(z = 0; z < newNewArr.length; z++){
-    number.push(newNewArr[z].reduce((a,b) => a + b))
-  }
-  let answer = Math.max(number)
-  console.log(answer[answer.length - 1])
+  }console.log(newArr)
+  console.log(arr)
+}
 
-  }
-high("the store is over there")
+removeEveryOther(["bob", "dave", "joe", "smith"])
