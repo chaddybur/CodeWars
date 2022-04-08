dc3fa45ed4cbf4ee333b4d9506d4fdcc2ee82f91
Chad Cornwell <chadcornwell86@yahoo.com>
2022-04-08 09:53:56 -0500

    THIRD TRY

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-
-function maskify(cc) {
-  let creditArr = cc.split("");
-  if( creditArr.length <= 4){
-    let answer = creditArr.join("")
-    return answer
-  }else{
-    for(i= 0; i<(creditArr.length - 4); i++){
-      creditArr[i] = '#'
-    }
-    console.log(creditArr.join(""))
+function findOdd(A) {
+	let newArr = []
+  let newObj = A.reduce(function(obj, item){
+		if (!obj[item]) {
+        obj[item] = 0;
+      };
+      obj[item]++;
+      return obj;
+    }, {});
+  console.log(newObj)
+  let newKeys = []
+  newKeys = Object.keys(newObj)
+  console.log(newKeys)
+ newArr = Object.values(newObj)
+  console.log(newArr)
+for(i = 0; i < newArr.length; i++){
+  if(newArr[i] % 2 !== 0){
+    return parseInt(newKeys[i])
   }
-
-  
-  
-
 }
-maskify('4556364607935616')
+  
+}
