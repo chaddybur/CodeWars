a441622501d748c117c8ee4cb8b7408d24d98313
Chad Cornwell <chadcornwell86@yahoo.com>
2022-04-10 15:27:16 -0500

    FIRST TRYish? After I understood the directions.

--- main.js
+++ main.js
@@ -4,29 +4,27 @@ sequence and returns a list of items without any elements with
 the same value next to each other and preserving the original order of elements.*/
 
 var uniqueInOrder=function(iterable){
-  let newArr = []
-  console.log(typeof iterable)
-  if(typeof iterable !== Array){
+  let newArr = [];
+  
+  if(typeof iterable === "string"){
     newArr = iterable.split("")
   }else{
     newArr = iterable
   }
-  console.log(newArr)
-
-let newObj = newArr.reduce((obj, item)=>{
-  if (!obj[item]) {
-    obj[item] = 0;
+  for(i = 0; i < (newArr.length - 1); i++){
+    if(newArr[i]=== newArr[i+1]){
+      newArr.splice((i+1), 1);
+      i--
+    }
   }
-  obj[item]++;
-  return obj;
-}, {});
-console.log(newObj)
-console.log(Object.keys(newObj))
+  console.log(newArr)
 
 }
 uniqueInOrder('AAAABBBCCDAABBB')
+uniqueInOrder('ABBCcAD') 
+uniqueInOrder([1,2,2,3,3])
+
 
 /*uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 uniqueInOrder([1,2,2,3,3])       == [1,2,3] */
-
