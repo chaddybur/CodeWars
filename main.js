ecafcd0ebaad46b6e0b6558fb68fcffb65454c48
Chad Cornwell <chadcornwell86@yahoo.com>
2022-04-14 10:11:39 -0500

    FIRST 5 KYUgit add .git add .

--- main.js
+++ main.js
@@ -1,47 +1,38 @@
-/*<6 Kyu> Dashatize it
-Given a variable n,
+/*<5 Kyu> Simple Pig Latin
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. 
+Leave punctuation marks untouched.
 
-If n is an integer, Return a string with dash'-'marks before and after each odd integer, but do not begin or end the string with a dash mark. If n is negative, then the negative sign should be removed.
+Examples
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !*/
 
-If n is not an integer, return an empty value.
-
-Ex:
-
-dashatize(274) -> '2-7-4'
-dashatize(6815) -> '68-1-5'*/
-
-function dashatize(num) {
-  //get 'em
-  if(typeof num !== "number"){
-    return NaN
-  }
+function pigIt(str){
+  //Code here
+  // grab each word into an array
+	let arr = str.split(" ")
+console.log(arr)
+  let newArr = arr.map(e => e.split(""))
+console.log(newArr)
+  //loop over the array to grab the first letter and push it to the back + "ay"
+  let ansArr= ''
+  for(i = 0; i< newArr.length; i++) {
+    if(newArr[i][0] !== '!' && newArr[i][0] !== "." && newArr[i][0] !== "?"){
+    console.log(newArr[i][0])
+    newArr[i].push(newArr[i][0]+'ay')
+    newArr[i].shift(newArr[i][0])
+    newArr[i] = newArr[i].join("")
+  }}
   
-  let arr = Math.abs(num).toString().split("")
- 
- let newArr = arr.map(e => parseInt(e))
-let lastNum = newArr[newArr.length -1 ]
-console.log(lastNum)
- if(lastNum % 2 !== 0){
-  newArr[newArr.length -1 ]= "-" + lastNum
- }else if (newArr[0] % 2 !== 0){
-   newArr[0] = newArr[0] + "-"
- }
- console.log(newArr)
-  for (i= 1; i < (newArr.length -1); i++){
-    if(newArr[i] % 2 !== 0){
-      newArr[i] = "-"+ newArr[i]+"-"
-    }
-  }let answerArr = newArr.join("").split("")
-  console.log(answerArr)
-  for(i= 1; i < (answerArr.length - 1); i++){
-    if(answerArr[i] === "-" && answerArr[(i+1)] === "-"){
-      answerArr[i] = ""
-    }
+  console.log(newArr)
+ansArr = newArr.join(" ")
+  
+  console.log(ansArr)
+return ansArr
+  // join the array
 
 }
-console.log(answerArr.join(""))
-return answerArr.join("")
-}
-
-dashatize(11327747)
-//2-7-4
+pigIt('O termpor o mores .')
+//'Oay', 'emporatay', 'oay', 'oresmay', '!ay'
+//pigIt('Pig latin is cool');
+pigIt('Hello world !');
+//pigIt('This is my string');
