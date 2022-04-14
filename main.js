65187d483b919afe0a42ce2e62ef7b06267ecbc4
Chad Cornwell <chadcornwell86@yahoo.com>
2022-04-14 13:59:06 -0500

    Got em

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
-function findOutlier(integers){
-  //your code here
-  let odds= 0
-  let evens = 0
-  let oddNumber = 0
-  let evenNumber = 0
-  for(i=0; i < integers.length; i++){
-    if(integers[i] % 2 === 0){
-      evens += 1
-      evenNumber = integers[i]
-      
-    }else{
-      odds += 1
-      oddNumber = integers[i]
+function duplicateCount(text){
+  //...
+  let arr = text.toLowerCase().split("")
+  console.log(arr)
+  let newObj = arr.reduce(function(obj, item) {
+      if (!obj[item]) {
+        obj[item] = 0;
+      }
+      obj[item]++;
+      return obj;
+    }, {});
+  console.log(newObj)
+  let numArr = Object.values(newObj)
+  console.log(numArr)
+  let counter= 0 
+  for (i = 0; i < numArr.length; i++){
+    if(numArr[i]> 1){
+      counter += 1
     }
-    
-    }
-  if(evens === 1){
-      return evenNumber
-    }else{
-      return oddNumber
   }
+  return counter
+ 
 }
-    findOutlier([0, 1, 2])
 
-    // 6 Kyu Find the Parity Outlier
+duplicateCount("abbcsdfecsxxxa")
+
+//6 KYU Counting Duplicates
