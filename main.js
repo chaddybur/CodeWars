37f84b7596b1aab9dcb62882016e23b0e43f535d
Chad Cornwell <chadcornwell86@yahoo.com>
2022-04-14 13:05:05 -0500

    first try

--- main.js
+++ main.js
@@ -1,19 +1,8 @@
-/* <5 Kyu> Move Zeros to the End
-Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
-
-moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]*/ 
-
-function moveZeros(arr) {
-  let counter = 0
-  for(i= 0; i < arr.length - counter; i++){
-    if(arr[i] === 0){
-      arr.push(arr[i])
-      counter += 1
-      arr.splice(i,1)
-    }
-  }
-  console.log(arr)
+function reverseWords(str) {
+  // Go for it
+  let arr = str.split(" ")
+  let newArr = arr.map(e => e.split("").reverse().join(""))
+  let answerStr = newArr.join(" ")
+  return answerStr
 }
-
-
-moveZeros([1,2,0,1,0,1,0,3,0,1])
+//easy 7Kyu Reverse
