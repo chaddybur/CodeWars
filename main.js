ec88745e5f4d8d8b402ce31c72dc916e58d4fde2
Chad Cornwell <chadcornwell86@yahoo.com>
2022-04-19 12:40:33 -0500

    6KYU first Try

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
-function alphabetPosition(text) {
-  
-  let newText = text.toLowerCase()
-  let textArr = newText.split("")
-  let answerArr = []
-  let alphArr = ["a","b","c","d","e","f","g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
- console.log(textArr)
-  for(i= 0; i<textArr.length; i++){
-    for (j = 0; j < alphArr.length; j++){
-      if(textArr[i] === alphArr[j]){
-        answerArr.push(j + 1)
+// 6 KYU differences in array
+
+function arrayDiff(a, b) {
+
+  if(a.length >= b.length){
+
+  for(i= 0 ; i < a.length; i++){
+    for (j= 0; j < b.length; j++){
+      if (a[i] === b[j]){
+        a.splice(i, 1)
+        i--
+        console.log(a)
       }
     }
-  }console.log(answerArr.join(" "))
-  
-}
+    
+    }
+    return a
+  }
+    else{
+      for(i= 0 ; i < b.length; i++){
+    for (j= 0; j < a.length; j++){
+       if (b[i] === a[j]){
+        b.splice(i, 1)
+         i--
+    }
+      }
+      }
+  return b
+}}
 
-alphabetPosition("The sunset sets at twelve o' clock.")
-                  //, "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11");
-alphabetPosition("The narwhal bacons at midnight.")
-  //, "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20");
+arrayDiff([1,2,3], [1,2])
