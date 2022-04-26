e28f6f307df3e2156946d716eef9fdf66905ec77
Chad Cornwell <chadcornwell86@yahoo.com>
2022-04-26 16:26:39 -0500

    First Try

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
-//6 KYU Two to One
-
-function longest(s1, s2) {
-  // your code
-  let con = s1.concat(s2)
-  let conArr = con.split("")
-  const sorted = conArr.reduce(function(obj, item) {
-      if (!obj[item]) {
-        obj[item] = 0;
-      }
-      obj[item]++;
-      return obj;
-    }, {});
-  let thing = Object.keys(sorted)
+function stringClean(s){
+  // Function will return the cleaned string
+  let nums = ['0','1','2','3','4','5','6','7','8','9']
+  let thing = s.split("")
   console.log(thing)
-  let answer = thing.sort();
-  return answer.join("")
-
+  for(i = 0; i < thing.length; i++){
+    for(j=0; j < nums.length; j++){
+      if(thing[i] == nums[j]){
+        thing.splice(i,1)
+        i--
+      }
+    }
+  }console.log(thing)
+  let ans = thing.join("")
+  console.log(ans)
+  
 }
 
-longest("aretheyhere", "yestheyarehere")
-//, "aehrsty")
+stringClean("Dsa32 cdsc34232 csa!!! 1I 4Am cool!")
