8d69bc0167663c89e8055d789d148e773e317a06
Chad Cornwell <chadcornwell86@yahoo.com>
2022-05-08 15:07:24 -0500

    first try

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-function checkForFactor (base, factor) {
-  if(base % factor == 0){
-    return true
+
+function well(x){
+  
+  let arr = x.filter(e=> e =="good")
+
+  if(arr.length == 0){
+    return "Fail!"
+  }else if(arr.length < 3){
+    return "Publish!"
+  }else{
+return "I smell a series!"
   }
-  return false
+  
+
 }
+
+well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good'])
+
+//, 'I smell a series!')
