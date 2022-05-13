ee0af2c97db566043bcfcd772ab4f7e7a8782768
Chad Cornwell <chadcornwell86@yahoo.com>
2022-05-13 16:49:22 -0500

    first try

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-// add the value "codewars" to the websites array 1,000 times
-
-let counter = 0
-var websites = []
-while (counter < 1000){
-  websites.push("codewars")
-  counter += 1
-}
-
-// creative one
+var cubeChecker = function(volume, side){  
+  if(side < 1){
+    return false
+  }
+  if(side ** 3 == volume) {
+    return true
+  }
+  return false
+};
