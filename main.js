f097e92ae87497b868ec55a630737b81ef342571
Chad Cornwell <chadcornwell86@yahoo.com>
2022-05-14 13:10:49 -0500

    thing

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ function converter (mpg) {
   //code to convert miles per imperial gallon to kilometers per liter
   
  let num = parseFloat(mpg / 4.54609188 * 1.609344).toFixed(2)
- return Number(num),
+ return Number(num)
 }
+
+// checking to see if it works
